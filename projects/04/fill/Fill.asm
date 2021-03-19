33be9ffcca2deb597d9c18264b3b11784c4cffe0
Rob Wheeler <rob.wheeler@gmail.com>
2021-03-19 08:03:23 -0700

    project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,48 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(EVENT)
+  @KBD
+  D=M
+  @FILL_BLACK
+  D;JNE
+  @R0
+  M=0
+  @FILL
+  0;JMP
+  (FILL_BLACK)
+    @R0
+    M=-1
+    @FILL
+    0;JMP
+  @EVENT
+  0;JMP
+
+(FILL)
+  @16384
+  D=A
+  @i
+  M=D
+  (LOOP)
+    @i
+    D=M
+    @24575
+    D=D-A
+    @EVENT  // while (i < 24576)
+    D;JGT
+
+    @R0
+    D=M
+    @i
+    A=M
+    M=D
+
+    @i
+    M=M+1  // ++i
+    @LOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2
+M=0
+
+@i
+M=1
+
+(LOOP)
+  @i
+  D=M
+  @R1
+  D=D-M   // for (i = 1; i <= R1; ++i)
+  @END
+  D;JGT
+
+  @R0
+  D=M
+  @R2
+  M=D+M  // R2 += R0
+
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
